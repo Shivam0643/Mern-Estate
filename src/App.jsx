3c90f9cfbc26574eed945f163c36c672e0b1d274
Shivam0643 <shivamlashkari246@gmail.com>
2025-07-31 13:14:54 +0530

    intsall reactjs and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-red-500 text-4xl font-bold">Hello shivam</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
