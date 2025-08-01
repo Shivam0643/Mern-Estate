149b65e81fd2cabddc7e51597ab7dd2ee6d76e2f
Shivam0643 <shivamlashkari246@gmail.com>
2025-08-01 14:49:40 +0530

    complete sign in page functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,5 @@
 export const test = (req, res) => {
     res.json({
-        message: "shicvam apii kwj ",
+        message: "app is working ",
     })
 };
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Home from "./pages/Home"
-import Signin from "./pages/Signin";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
+import SignIn from "./pages/Signin";
+
 
 function App() {
   return <BrowserRouter>
   <Header/>
   <Routes>
     <Route path="/" element={<Home/>}/>
-    <Route path="/sign-in" element={<Signin/>}/>
+    <Route path="/sign-in" element={<SignIn/>}/>
     <Route path="/sign-up" element={<SignUp/>}/>
     <Route path="/about" element={<About/>}/>
     <Route path="/profile" element={<Profile/>}/>
