f8e2bb8f365b251dc2c5fd80ad150c0bca4c832b
Shivam0643 <shivamlashkari246@gmail.com>
2025-08-01 13:33:44 +0530

    complete sign up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
+  },
+  plugins: [tailwindcss()],
+});
